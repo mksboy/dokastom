bf5cc96c996745fd550fc048dcc944f56f372dc7
BezrukovBoris <bezb87@gmail.com>
2023-10-02 15:51:12 +0300

    ds_topmenu_dropdown.tpl edit

--- js/addons/_dokastom/ds_wishlist.js
+++ js/addons/_dokastom/ds_wishlist.js
@@ -5,25 +5,24 @@
 
     function fn_ds_wishlist_add() {
         let ds_span = $('#ds_wishlist_span');
-        let ds_wishlist_let = 0;
         // let ds_wishlist_var = (isNaN(ds_span.html()) ? Number(0) : Number(ds_span.html()) )
-        let ds_wishlist_var =  Number(ds_span.html());
-        if (!ds_wishlist_var) {
-            $('#ds_wishlist_span').html('1')
-        } else {
-            ds_wishlist_let = ds_wishlist_var;
-        }
-        // let ds_wishlist_var = Number($('#ds_wishlist_span').html());
+        // let ds_wishlist_var =  Number(ds_span.html());
+        // if (!ds_wishlist_var) {
+        //     $('#ds_wishlist_span').html('1')
+        // } else {
+        //     ds_wishlist_let = ds_wishlist_var;
+        // }
+        let ds_wishlist_var = Number(ds_span.html());
 
         let ds_wishlist_data = $(this).data('ds-dispatch');
         if (!$(this).hasClass('ds_wishlist_in')) {
             $(this).addClass('ds_wishlist_in');
-            ds_wishlist_let += 1;
+            ds_wishlist_var += 1;
         } else {
             $(this).removeClass('ds_wishlist_in');
-            ds_wishlist_let -= 1;
+            ds_wishlist_var -= 1;
         }
-        ds_span.html(ds_wishlist_let);
+        ds_span.html(ds_wishlist_var);
 
         $.ceAjax('request', fn_url("wishlist.add"), {
             method : 'post',
